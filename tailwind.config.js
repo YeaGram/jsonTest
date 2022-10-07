81cbce0eea1f836baf1fd30dc00da65701f0eca0
YeaGram <managerdims@gmail.com>
2022-10-07 22:29:14 +0800

    huh

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       dropShadow: {
         myDropOne: "0px 3px 5px rgba(0,0,0,0.2)",
-        myDropTwo: "0px 20px 10px rgba(0,0,0,0.3)",
+        myDropTwo: "0px 30px 10px rgba(0,0,0,0.3)",
       },
       fontFamily: {
         tilti: "'Titillium Web'",
